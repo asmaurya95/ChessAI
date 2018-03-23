3a3168c5c4a657cebd7879c3ee4a5f80fe454c78
ashutosh <ashutosh@maurya-g50>
2018-03-23 09:33:36 +0530

    Move Generation upto King's Possible Moves

--- src/ChessAI.java
+++ src/ChessAI.java
@@ -136,6 +136,39 @@ public class ChessAI {
     }
 
     public static String possibleMovesQueen(int i) {
-        return "";
+        String list = "", oldPiece;
+        int r = i / 8, c = i % 8;
+        int temp = 1;
+        for (int j = -1; j <= 1; j++) {
+            for (int k = -1; k <= 1; k++) {
+                try {
+                    while (" ".equals(chessBoard[r + temp * j][c + temp * k])) {
+                        oldPiece = chessBoard[r + temp * j][c + temp * k];
+                        chessBoard[r][c] = " ";
+                        chessBoard[r + temp * j][c + temp * k] = "Q";
+                        if (isKingSafe()) {
+                            list = list + r + c + (r + temp * j) + (c + temp * k) + oldPiece;
+                        }
+                        chessBoard[r][c] = "Q";
+                        chessBoard[r + temp * j][c + temp * k] = oldPiece;
+                        temp++;
+                    }
+                    if (Character.isLowerCase(chessBoard[r + temp * j][c + temp * k].charAt(0))) {
+                        oldPiece = chessBoard[r + temp * j][c + temp * k];
+                        chessBoard[r][c] = " ";
+                        chessBoard[r + temp * j][c + temp * k] = "Q";
+                        if (isKingSafe()) {
+                            list = list + r + c + (r + temp * j) + (c + temp * k) + oldPiece;
+                        }
+                        chessBoard[r][c] = "Q";
+                        chessBoard[r + temp * j][c + temp * k] = oldPiece;
+
+                    }
+                } catch (Exception e) {
+                }
+                temp = 1;
+            }
+        }
+        return list;
     }
 }
