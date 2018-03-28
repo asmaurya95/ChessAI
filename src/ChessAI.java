fd8b1132df1dc44d2be220e243984799dd930f91
ashutosh <ashutosh@maurya-g50>
2018-03-28 07:56:53 +0530

    King Safety Added

--- src/ChessAI.java
+++ src/ChessAI.java
@@ -299,6 +299,94 @@ public class ChessAI {
     }
 
     public static boolean isKingSafe() {
+        //bishop/queen
+        int temp = 1;
+        for (int i = -1; i <= 1; i += 2) {
+            for (int j = -1; j <= 1; j += 2) {
+                try {
+                    while (" ".equals(chessBoard[kingPosC / 8 + temp * i][kingPosC % 8 + temp * j])) {
+                        temp++;
+                    }
+                    if ("b".equals(chessBoard[kingPosC / 8 + temp * i][kingPosC % 8 + temp * j])
+                            || "q".equals(chessBoard[kingPosC / 8 + temp * i][kingPosC % 8 + temp * j])) {
+                        return false;
+                    }
+                } catch (Exception e) {
+                }
+                temp = 1;
+            }
+        }
+        //rook/queen
+        for (int i = -1; i <= 1; i += 2) {
+            try {
+                while (" ".equals(chessBoard[kingPosC / 8][kingPosC % 8 + temp * i])) {
+                    temp++;
+                }
+                if ("r".equals(chessBoard[kingPosC / 8][kingPosC % 8 + temp * i])
+                        || "q".equals(chessBoard[kingPosC / 8][kingPosC % 8 + temp * i])) {
+                    return false;
+                }
+            } catch (Exception e) {
+            }
+            temp = 1;
+            try {
+                while (" ".equals(chessBoard[kingPosC / 8 + temp * i][kingPosC % 8])) {
+                    temp++;
+                }
+                if ("r".equals(chessBoard[kingPosC / 8 + temp * i][kingPosC % 8])
+                        || "q".equals(chessBoard[kingPosC / 8 + temp * i][kingPosC % 8])) {
+                    return false;
+                }
+            } catch (Exception e) {
+            }
+            temp = 1;
+        }
+        //knight
+        for (int i = -1; i <= 1; i += 2) {
+            for (int j = -1; j <= 1; j += 2) {
+                try {
+                    if ("k".equals(chessBoard[kingPosC / 8 + i][kingPosC % 8 + j * 2])) {
+                        return false;
+                    }
+                } catch (Exception e) {
+                }
+                try {
+                    if ("k".equals(chessBoard[kingPosC / 8 + i * 2][kingPosC % 8 + j])) {
+                        return false;
+                    }
+                } catch (Exception e) {
+                }
+            }
+        }
+        //pawn
+        if (kingPosC >= 16) {
+            try {
+                if ("p".equals(chessBoard[kingPosC / 80 - 1][kingPosC % 8 - 1])) {
+                    return false;
+                }
+            } catch (Exception e) {
+            }
+            try {
+                if ("p".equals(chessBoard[kingPosC / 80 - 1][kingPosC % 8 + 1])) {
+                    return false;
+                }
+            } catch (Exception e) {
+            }
+            //king
+            for (int i = -1; i <= 1; i++) {
+                for (int j = -1; j <= 1; j++) {
+                    if (i != 0 || j != 0) {
+                        try {
+                            if ("a".equals(chessBoard[kingPosC / 8 + i][kingPosC % 8 + j])) {
+                                return false;
+                            }
+                        } catch (Exception e) {
+                        }
+                    }
+                }
+            }
+        }
+
         return true;
     }
 }
