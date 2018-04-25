462e5d3fa596a9d369b2bca38272ee2b245f2ece
ashutosh <ashutosh@maurya-g50>
2018-04-25 14:08:34 +0530

    Completed

--- src/Pawn.java
+++ src/Pawn.java
@@ -21,27 +21,27 @@ public class Pawn {
                 if (ChessAI.isOpponent(ChessAI.chessBoard[r - 1][c + j].charAt(0)) && i >= 16) {
                     oldPiece = ChessAI.chessBoard[r - 1][c + j];
                     ChessAI.chessBoard[r][c] = " ";
-                    ChessAI.chessBoard[r - 1][c + j] = "P";
+                    ChessAI.chessBoard[r - 1][c + j] = pawn();
                     if (King.isKingSafe()) {
                         list = list + r + c + (r - 1) + (c + j) + oldPiece;
                     }
-                    ChessAI.chessBoard[r][c] = "P";
+                    ChessAI.chessBoard[r][c] = pawn();
                     ChessAI.chessBoard[r - 1][c + j] = oldPiece;
                 }
             } catch (Exception e) {
             }
             try {//promotion && capture
                 if (Character.isLowerCase(ChessAI.chessBoard[r - 1][c + j].charAt(0)) && i < 16) {
-                    String[] temp = {"Q", "R", "B", "K"};
+                    String[] temp = {Queen.queen(), Rook.rook(), Bishop.bishop(), Knight.knight()};
                     for (int k = 0; k < 4; k++) {
                         oldPiece = ChessAI.chessBoard[r - 1][c + j];
                         ChessAI.chessBoard[r][c] = " ";
                         ChessAI.chessBoard[r - 1][c + j] = temp[k];
                         if (King.isKingSafe()) {
                             //column1,column2,captured-piece,new-piece,P
-                            list = list + c + (c + j) + oldPiece + temp[k] + "P";
+                            list = list + c + (c + j) + oldPiece + temp[k] + pawn();
                         }
-                        ChessAI.chessBoard[r][c] = "P";
+                        ChessAI.chessBoard[r][c] = pawn();
                         ChessAI.chessBoard[r - 1][c + j] = oldPiece;
                     }
                 }
@@ -52,27 +52,27 @@ public class Pawn {
             if (" ".equals(ChessAI.chessBoard[r - 1][c]) && i >= 16) {
                 oldPiece = ChessAI.chessBoard[r - 1][c];
                 ChessAI.chessBoard[r][c] = " ";
-                ChessAI.chessBoard[r - 1][c] = "P";
+                ChessAI.chessBoard[r - 1][c] = pawn();
                 if (King.isKingSafe()) {
                     list = list + r + c + (r - 1) + c + oldPiece;
                 }
-                ChessAI.chessBoard[r][c] = "P";
+                ChessAI.chessBoard[r][c] = pawn();
                 ChessAI.chessBoard[r - 1][c] = oldPiece;
             }
         } catch (Exception e) {
         }
         try {//promotion && no capture
             if (" ".equals(ChessAI.chessBoard[r - 1][c]) && i < 16) {
-                String[] temp = {"Q", "R", "B", "K"};
+                String[] temp = {Queen.queen(), Rook.rook(), Bishop.bishop(), Knight.knight()};
                 for (int k = 0; k < 4; k++) {
                     oldPiece = ChessAI.chessBoard[r - 1][c];
                     ChessAI.chessBoard[r][c] = " ";
                     ChessAI.chessBoard[r - 1][c] = temp[k];
                     if (King.isKingSafe()) {
                         //column1,column2,captured-piece,new-piece,P
-                        list = list + c + c + oldPiece + temp[k] + "P";
+                        list = list + c + c + oldPiece + temp[k] + pawn();
                     }
-                    ChessAI.chessBoard[r][c] = "P";
+                    ChessAI.chessBoard[r][c] = pawn();
                     ChessAI.chessBoard[r - 1][c] = oldPiece;
                 }
             }
@@ -82,11 +82,11 @@ public class Pawn {
             if (" ".equals(ChessAI.chessBoard[r - 1][c]) && " ".equals(ChessAI.chessBoard[r - 2][c]) && i >= 48) {
                 oldPiece = ChessAI.chessBoard[r - 2][c];
                 ChessAI.chessBoard[r][c] = " ";
-                ChessAI.chessBoard[r - 2][c] = "P";
+                ChessAI.chessBoard[r - 2][c] = pawn();
                 if (King.isKingSafe()) {
                     list = list + r + c + (r - 2) + c + oldPiece;
                 }
-                ChessAI.chessBoard[r][c] = "P";
+                ChessAI.chessBoard[r][c] = pawn();
                 ChessAI.chessBoard[r - 2][c] = oldPiece;
             }
         } catch (Exception e) {
