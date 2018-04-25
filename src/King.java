cfb7c0b50f5a2c84d4ab419e8fc153e046a6bb26
ashutosh <ashutosh@maurya-g50>
2018-04-25 20:28:21 +0530

    Completed

--- src/King.java
+++ src/King.java
@@ -5,14 +5,6 @@
  */
 public class King {
 
-    public static String king() {
-        if (ChessAI.user.equals("X")) {
-            return "a";
-        } else {
-            return "A";
-        }
-    }
-
     public static String possibleMovesKing(int i) {
         String list = "", oldPiece;
         int r = i / 8, c = i % 8;
@@ -20,10 +12,10 @@ public class King {
             if (j != 4) {
                 try {
                     String ch = ChessAI.chessBoard[r - 1 + j / 3][c - 1 + j % 3];
-                    if (ChessAI.isOpponent(ch.charAt(0)) || " ".equals(ch)) {
+                    if (Character.isLowerCase(ch.charAt(0)) || " ".equals(ch)) {
                         oldPiece = ch;
                         ChessAI.chessBoard[r][c] = " ";
-                        ChessAI.chessBoard[r - 1 + j / 3][c - 1 + j % 3] = king();
+                        ChessAI.chessBoard[r - 1 + j / 3][c - 1 + j % 3] = "A";
                         int temp = ChessAI.kingPosC;
                         ChessAI.kingPosC = i + (j / 3) * 8 + j % 3 - 9;
                         if (isKingSafe()) {
@@ -32,7 +24,7 @@ public class King {
                                     + Integer.toString(c - 1 + j % 3)
                                     + oldPiece;
                         }
-                        ChessAI.chessBoard[r][c] = king();
+                        ChessAI.chessBoard[r][c] = "A";
                         ChessAI.chessBoard[r - 1 + j / 3][c - 1 + j % 3] = oldPiece;
                         ChessAI.kingPosC = temp;
                     }
@@ -52,8 +44,8 @@ public class King {
                     while (" ".equals(ChessAI.chessBoard[ChessAI.kingPosC / 8 + temp * i][ChessAI.kingPosC % 8 + temp * j])) {
                         temp++;
                     }
-                    if (Bishop.opponentBishop().equals(ChessAI.chessBoard[ChessAI.kingPosC / 8 + temp * i][ChessAI.kingPosC % 8 + temp * j])
-                            || Queen.opponentQueen().equals(ChessAI.chessBoard[ChessAI.kingPosC / 8 + temp * i][ChessAI.kingPosC % 8 + temp * j])) {
+                    if ("b".equals(ChessAI.chessBoard[ChessAI.kingPosC / 8 + temp * i][ChessAI.kingPosC % 8 + temp * j])
+                            || "q".equals(ChessAI.chessBoard[ChessAI.kingPosC / 8 + temp * i][ChessAI.kingPosC % 8 + temp * j])) {
                         return false;
                     }
                 } catch (Exception e) {
@@ -67,8 +59,8 @@ public class King {
                 while (" ".equals(ChessAI.chessBoard[ChessAI.kingPosC / 8][ChessAI.kingPosC % 8 + temp * i])) {
                     temp++;
                 }
-                if (Rook.opponentRook().equals(ChessAI.chessBoard[ChessAI.kingPosC / 8][ChessAI.kingPosC % 8 + temp * i])
-                        || Queen.opponentQueen().equals(ChessAI.chessBoard[ChessAI.kingPosC / 8][ChessAI.kingPosC % 8 + temp * i])) {
+                if ("r".equals(ChessAI.chessBoard[ChessAI.kingPosC / 8][ChessAI.kingPosC % 8 + temp * i])
+                        || "q".equals(ChessAI.chessBoard[ChessAI.kingPosC / 8][ChessAI.kingPosC % 8 + temp * i])) {
                     return false;
                 }
             } catch (Exception e) {
@@ -78,8 +70,8 @@ public class King {
                 while (" ".equals(ChessAI.chessBoard[ChessAI.kingPosC / 8 + temp * i][ChessAI.kingPosC % 8])) {
                     temp++;
                 }
-                if (Rook.opponentRook().equals(ChessAI.chessBoard[ChessAI.kingPosC / 8 + temp * i][ChessAI.kingPosC % 8])
-                        || Queen.opponentQueen().equals(ChessAI.chessBoard[ChessAI.kingPosC / 8 + temp * i][ChessAI.kingPosC % 8])) {
+                if ("r".equals(ChessAI.chessBoard[ChessAI.kingPosC / 8 + temp * i][ChessAI.kingPosC % 8])
+                        || "q".equals(ChessAI.chessBoard[ChessAI.kingPosC / 8 + temp * i][ChessAI.kingPosC % 8])) {
                     return false;
                 }
             } catch (Exception e) {
@@ -90,13 +82,13 @@ public class King {
         for (int i = -1; i <= 1; i += 2) {
             for (int j = -1; j <= 1; j += 2) {
                 try {
-                    if (Knight.opponentKnight().equals(ChessAI.chessBoard[ChessAI.kingPosC / 8 + i][ChessAI.kingPosC % 8 + j * 2])) {
+                    if ("k".equals(ChessAI.chessBoard[ChessAI.kingPosC / 8 + i][ChessAI.kingPosC % 8 + j * 2])) {
                         return false;
                     }
                 } catch (Exception e) {
                 }
                 try {
-                    if (Knight.opponentKnight().equals(ChessAI.chessBoard[ChessAI.kingPosC / 8 + i * 2][ChessAI.kingPosC % 8 + j])) {
+                    if ("k".equals(ChessAI.chessBoard[ChessAI.kingPosC / 8 + i * 2][ChessAI.kingPosC % 8 + j])) {
                         return false;
                     }
                 } catch (Exception e) {
@@ -106,13 +98,13 @@ public class King {
         //pawn
         if (ChessAI.kingPosC >= 16) {
             try {
-                if (Pawn.opponentPawn().equals(ChessAI.chessBoard[ChessAI.kingPosC / 80 - 1][ChessAI.kingPosC % 8 - 1])) {
+                if ("p".equals(ChessAI.chessBoard[ChessAI.kingPosC / 80 - 1][ChessAI.kingPosC % 8 - 1])) {
                     return false;
                 }
             } catch (Exception e) {
             }
             try {
-                if (Pawn.opponentPawn().equals(ChessAI.chessBoard[ChessAI.kingPosC / 80 - 1][ChessAI.kingPosC % 8 + 1])) {
+                if ("p".equals(ChessAI.chessBoard[ChessAI.kingPosC / 80 - 1][ChessAI.kingPosC % 8 + 1])) {
                     return false;
                 }
             } catch (Exception e) {
@@ -122,7 +114,7 @@ public class King {
                 for (int j = -1; j <= 1; j++) {
                     if (i != 0 || j != 0) {
                         try {
-                            if (opponentKing().equals(ChessAI.chessBoard[ChessAI.kingPosC / 8 + i][ChessAI.kingPosC % 8 + j])) {
+                            if ("a".equals(ChessAI.chessBoard[ChessAI.kingPosC / 8 + i][ChessAI.kingPosC % 8 + j])) {
                                 return false;
                             }
                         } catch (Exception e) {
@@ -132,13 +124,5 @@ public class King {
             }
         }
         return true;
-    }
-
-    public static String opponentKing() {
-        if (ChessAI.user.equals("X")) {
-            return "A";
-        } else {
-            return "a";
-        }
     }
 }
